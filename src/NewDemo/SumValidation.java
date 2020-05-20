ce98d5d39bc39d77e5054e243fd27c6ae343ef86
Vishvesh Sawant <vishvesh.sawant@mirriad.com>
2020-05-20 19:45:39 +0530

    Added Sum Validation code

--- src/NewDemo/SumValidation.java
+++ src/NewDemo/SumValidation.java
@@ -1,5 +1,5 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import io.restassured.path.json.JsonPath;
 
 public class SumValidation {
@@ -7,14 +7,20 @@ public class SumValidation {
 	@Test
 	public void SumCourses() {
 		
-		JsonPath js = new JsonPath(payLoad.courseData());
-		int totalCourses = js.getInt("courses.size()");
-		
-		for (int i = 0; i < totalCourses; i++) {
-			int price = js.getInt("totalCourses["+i+"].price");
-			int copies = js.getInt("totalCourses["+i+"].copies");
-			int amount = price*copies;
+		JsonPath js3 = new JsonPath(payLoad.courseData());
+		int totalCourses = js3.getInt("courses.size()");
+		int purchaseAmount = js3.getInt("dashboard.purchaseAmount");
+		System.out.println(purchaseAmount);
+		int amount,finalAmount = 0;
+		for (int i=0;i<totalCourses;i++) {
+			int prices = js3.getInt("courses["+i+"].price");
+			System.out.println(prices);
+			int copies = js3.getInt("courses["+i+"].copies");
+			amount = prices*copies;
+			System.out.println("Total price is "+amount+"");
+			finalAmount = finalAmount + amount;
 		}
+		System.out.println("Total price is "+finalAmount+" for all copies");
+		Assert.assertEquals(purchaseAmount,finalAmount);	
 	}
-
 }
