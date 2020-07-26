69fa9d3f86cfe79cd5008041f59e723ee43e6038
Vishvesh14 <vishveshsavant@gmail.com>
2020-07-26 20:47:31 +0530

    added new file for google api add place endpoint serialization

---
+++ src/NewDemo/SerializationPojoTest/googleMapsAddPlace.java
@@ -0,0 +1,14 @@
+package SerializationPojoTest;
+
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+
+public class googleMapsAddPlace {
+
+	public static void main(String[] args) {
+		
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		given().
+	}
+
+}
