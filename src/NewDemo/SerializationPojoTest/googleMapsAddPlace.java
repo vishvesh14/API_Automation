8e8724913c0dee2bb568a2608c3d3beaf2a1017a
Vishvesh14 <vishveshsavant@gmail.com>
2020-08-02 01:07:51 +0530

    Serialization - Added Main and Subclasses

--- src/NewDemo/SerializationPojoTest/googleMapsAddPlace.java
+++ src/NewDemo/SerializationPojoTest/googleMapsAddPlace.java
@@ -1,6 +1,8 @@
 package SerializationPojoTest;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.given;
 
 public class googleMapsAddPlace {
@@ -8,7 +10,13 @@ public class googleMapsAddPlace {
 	public static void main(String[] args) {
 		
 		RestAssured.baseURI = "https://rahulshettyacademy.com";
-		given().
+		
+		 String res = given().queryParam("key", "qaclick123")
+		.body()
+		.when().post("/maps/api/place/add/json")
+		.then().assertThat().statusCode(200).extract().response().asString();
+		 
+		 System.out.println(res);
 	}
 
 }
