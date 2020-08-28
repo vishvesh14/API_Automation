21d67cac855111c5acffa0e53f30f315e6423b79
Vishvesh14 <vishveshsavant@gmail.com>
2020-08-28 06:40:44 +0530

    Added the API Automation Framework Project to master

---
+++ ApiAutomationFramework/src/test/java/resources/Utils.java
@@ -0,0 +1,48 @@
+package resources;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Properties;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class Utils {
+	
+	public static RequestSpecification req; //Variable is set to static, so that everytime a new variable instance is not created during the entire testcase.
+	public io.restassured.specification.RequestSpecification RequestSpecification() throws IOException{
+		if(req==null) {
+		
+		//Need to add a object to printstream class to tell clearly where to log the output.
+		//To create a file at runtime, New FileOutput Stream class
+		PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
+		//Spec Builder Request and Response are used, so to avoid writing the repeated code
+		req = new RequestSpecBuilder().setBaseUri(getGlobalValue("BaseURL"))
+				.addQueryParam("key","qaclick123")
+				.addFilter(RequestLoggingFilter.logRequestTo(log)) //adding logs at a global level, so that you dont need to put this code again and again for different test cases
+				.addFilter(ResponseLoggingFilter.logResponseTo(log)) //Log the response as well
+				.setContentType(ContentType.JSON)
+				.build();
+		return req;
+		}
+		return req;
+		
+	}
+	
+	
+	public static String getGlobalValue(String key) throws IOException {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream("src/test/java/resources/Global.properties");
+		prop.load(fis); //This loads the file to read
+		return prop.getProperty(key);
+	}
+	
+
+}
---
+++ ApiAutomationFramework/target/test-classes/feature/placeValidation.feature
@@ -0,0 +1,13 @@
+Feature: Validating Add Place API's
+
+Scenario Outline: Verify if place is successfully added using AddPlace API
+Given Add Place Payload with "<name>" "<address>" "<language>"
+When user calls "AddPlaceAPI" using the POST http request
+Then The API call is succesful with status code 200
+And "status" in response body is "OK"
+And "scope" in response body is "APP"
+
+Examples:
+		|name 			| address 			|language |
+		|Backyard house | 90, Main Street	|German	  |
+		|Front Grass House| 60, BackSide 	|Portuguese|	
